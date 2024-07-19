a1bc37364c9255d28f89f574347a839f4daa0312
Gisinho <112932133+Gisinho@users.noreply.github.com>
2024-07-19 09:10:30 +0200

    aggiunta scelte nel menu

--- progettoFinaleMain.py
+++ progettoFinaleMain.py
@@ -7,54 +7,58 @@ def menu():
 
     while True:
         print("\nMenu:")
-        print("1. Mostra informazioni sul dataset")
-        print("2. Mostra descrizione del dataset")
-        print("3. Gestisci valori mancanti")
-        print("4. Pulisci dati")
-        print("5. Correggi anomalie nei dati")
-        print("6. Crea colonne aggiuntive")
-        print("7. Analizza relazioni con Churn")
-        print("8. Analizza relazioni con Età")
-        print("9. Analizza relazioni con Durata Abbonamento")
-        print("10. Analizza relazioni con Tariffa Mensile")
-        print("11. Identifica correlazioni")
-        print("12. Converti Churn in formato numerico")
-        print("13. Normalizza colonne numeriche preparandole alla modellazione")
+        print("1. Mostra dataset")
+        print("2. Mostra informazioni sul dataset")
+        print("3. Mostra descrizione del dataset")
+        print("4. Distribuzione valori")
+        print("5. Gestisci valori mancanti")
+        print("6. Pulisci dati")
+        print("7. Correggi anomalie nei dati")
+        print("8. Crea colonne aggiuntive")
+        print("9. Analizza relazioni con Età")
+        print("10. Analizza relazioni con Durata Abbonamento")
+        print("11. Analizza relazioni con Tariffa Mensile")
+        print("12. Analizza relazioni Churn")
+        print("13. Identifica correlazioni")
+        print("14. Converti Churn in formato numerico")
+        print("15. Normalizza colonne numeriche preparandole alla modellazione")
         print("0. Esci")
 
         try:
-            scelta = int(input("Scegli un'opzione: "))
-            
+            scelta = int(input("\nScegli un'opzione: "))
             if scelta == 1:
+                print(df)
+            if scelta == 2:
                 pf.info(df)
-            elif scelta == 2:
-                pf.descrizione(df)
             elif scelta == 3:
-                pf.valori_mancanti(df)
+                pf.descrizione(df)
             elif scelta == 4:
+                pf.distribuzione_val(df)
+            elif scelta == 5:
+                pf.valori_mancanti(df)
+            elif scelta == 6:
                 df = pf.pulisci_dati(df)
                 print("Dati puliti.")
-            elif scelta == 5:
+            elif scelta == 7:
                 df = pf.correggi_anomalie(df)
                 print("Anomalie corrette.")
-            elif scelta == 6:
+            elif scelta == 8:
                 df = pf.crea_colonne(df)
                 print("Colonne aggiuntive create.")
-            elif scelta == 7:
-                pf.relazioni_churn(df)
-            elif scelta == 8:
-                pf.relazioni_eta(df)
             elif scelta == 9:
-                pf.relazioni_abbonamento(df)
+                pf.relazioni_eta(df)
             elif scelta == 10:
-                pf.relazioni_tariffa(df)
+                pf.relazioni_abbonamento(df)
             elif scelta == 11:
-                pf.identifica_correlazioni(df)
+                pf.relazioni_tariffa(df)
             elif scelta == 12:
-                pf.converti_churn(df)
+                pf.relazioni_churn(df)
             elif scelta == 13:
+                pf.identifica_correlazioni(df)
+            elif scelta == 14:
+                pf.converti_churn(df)
+            elif scelta == 15:
                 pf.normalizza_colonne_numeriche(df)
-                print("Hai normalizzato le colonne")
             elif scelta == 0:
                 print("Arrivederci")
                 break
